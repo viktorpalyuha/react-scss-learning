96f9c7e63bc2530c0eda4287d1e74994ddbe9b42
Viktor Palyuha <viktorpalyuha@gmail.com>
2020-03-12 19:01:29 +0200

    Learning scss and modules in react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.scss';
+
+console.log(classes);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={classes.App}>
+      <header className={classes['App-header']}>
+        <img src={logo} className={classes['App-logo']} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={classes['App-link']}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
